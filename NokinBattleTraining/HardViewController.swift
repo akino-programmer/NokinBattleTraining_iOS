9209c08a075d0215d6989e64816509233afc35b6
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-07-23 20:37:20 +0900

    move to EasyView, NormalView, HardView.

---
+++ NokinBattleTraining/HardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HardViewController.swift
+//  NokinBattleTraining
+//
+//  Created by 野口晃 on 2020/07/23.
+//  Copyright © 2020 Akira Noguchi. All rights reserved.
+//
+
+import UIKit
+
+class HardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func back(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
