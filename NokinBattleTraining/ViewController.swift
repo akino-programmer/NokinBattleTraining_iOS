f6c814638d20955d701876f7ee1f227d1bfae544
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-07-23 17:36:57 +0900

    Initial Commit

---
+++ NokinBattleTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NokinBattleTraining
+//
+//  Created by 野口晃 on 2020/07/23.
+//  Copyright © 2020 Akira Noguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
