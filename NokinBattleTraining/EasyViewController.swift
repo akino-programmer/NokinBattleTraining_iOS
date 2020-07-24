83f5ca3c0c4bfe3c27b09c8ebcf141f487903d94
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-07-24 18:23:41 +0900

    create function of damage to monsterHp.

--- NokinBattleTraining/EasyViewController.swift
+++ NokinBattleTraining/EasyViewController.swift
@@ -14,22 +14,85 @@ class EasyViewController: UIViewController {
     
     let maxHp = Int.random(in: 30 ..< 50) //0から9までで整数(Int)の乱数
     
+    var playTrainingTimes:[Int] = []
+    
+    var secondRemainHp = 0
+    
+    @IBOutlet weak var traningName: UILabel!
+    
     @IBOutlet weak var monsterMaxHp: UILabel!
     
+    @IBOutlet weak var monsterRemainHp: UILabel!
+    
+    @IBOutlet weak var traningTimes: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+      
+
         
       
     }
+    
+    
+    @IBAction func trainingStart(_ sender: Any) {
+        
+        let randomTraningTimes = Int.random(in: 10 ..< 20) //10から20までで整数(Int)の乱数
+        
+        if (monsterRemainHp.text == "ー"){
+            
+         monsterMaxHp.text = String(maxHp)
+         monsterRemainHp.text = String(maxHp)
+        
+        } else {
+            
+            secondRemainHp = Int(monsterRemainHp.text!)! - (playTrainingTimes.last!)
+            
+            monsterRemainHp.text = String(secondRemainHp)
+            
+        }
+        
+        //トレーニング回数を配列に保存
+        playTrainingTimes.append(randomTraningTimes)
+        
+        
+        print(playTrainingTimes)
+        
+        //トレーニング回数を表示
+        traningTimes.text = String(randomTraningTimes)
+        
+        
+        
+        
+        
+        //トレーニングメニュー配列からランダムに取り出す
+        let traningMenus = ["腕立て伏せ", "腹筋", "スクワット", "懸垂"]
+        let randomTraningMenu = traningMenus.randomElement()
+        
+        traningName.text = String(randomTraningMenu!)
+
+        
+
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     
     @IBAction func back(_ sender: Any) {
         
         dismiss(animated: true, completion: nil)
         
     }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
